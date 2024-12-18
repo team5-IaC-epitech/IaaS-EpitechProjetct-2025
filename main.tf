f1ecdca71d6119ca357a7fb59ffd8cb06dcb87e8
Marko <marko@localhost.localdomain>
2024-12-18 13:15:38 +0100

    cluster creation

--- main.tf
+++ main.tf
@@ -27,6 +27,31 @@ resource "google_compute_subnetwork" "vpc_subnet" {
   ip_cidr_range = var.cidr_block
   network       = google_compute_network.vpc_network.id
   region        = var.region
+
+  secondary_ip_range {
+    range_name = "pods"
+    ip_cidr_range = "10.0.2.0/24"
+  }
+  secondary_ip_range {
+    range_name = "services"
+    ip_cidr_range = "10.0.3.0/24"
+  }
+}
+
+resource "google_container_cluster" "autopilot" {
+  name = var.cluster_name
+  location = var.region
+  enable_autopilot = true
+  network = google_compute_network.vpc_network.id
+  subnetwork = google_compute_subnetwork.vpc_subnet.id
+  ip_allocation_policy {
+    cluster_secondary_range_name = "pods"
+    services_secondary_range_name = "services"
+  }
+  release_channel {
+    channel = "REGULAR"
+  }
+  deletion_protection = false
 }
 
 output "vpc_id" {
@@ -35,4 +60,12 @@ output "vpc_id" {
 
 output "subnet_id" {
   value = google_compute_subnetwork.vpc_subnet.id
+}
+
+output "cluster_name" {
+  value = google_container_cluster.autopilot.name
+}
+
+output "endpoint" {
+  value = google_container_cluster.autopilot.endpoint
 }
