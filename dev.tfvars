e942677ab2c74941242ece2f40c143eca4b6524d
Tokuuuu <72007811+iTokubz@users.noreply.github.com>
2025-12-12 13:55:27 +0100

    feat/kubclusters terraform file

--- dev.tfvars
+++ dev.tfvars
@@ -2,3 +2,14 @@ project_id  = "iaasepitech"
 region      = "europe-west9"
 vpc_name = "team5-vpc-dev"
 cidr_block  = "10.0.1.0/24"
+
+cluster_name = "team5-gke-cluster"
+environnment = "dev"
+node_count = 2
+min_node_count = 1
+max_node_count = 5
+machine_type = "e2-medium"
+disk_size_gb = 1
+disk_type = "pd-balanced"
+preemptible = false
+create_system_node_pool = false
--- variables.tf
+++ variables.tf
@@ -21,3 +21,63 @@ variable "cidr_block" {
   description = "CIDR block for the VPC"
   default     = "10.0.1.0/24"
 }
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+  default = "team5-gke-cluster"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the GKE cluster"
+  type        = number
+  default     = 2
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes in the GKE cluster"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes in the GKE cluster"
+  type        = number
+  default     = 5
+}
+
+variable "machine_type" {
+  description = "Machine type for GKE nodes"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "disk_size_gb" {
+  description = "Disk size for GKE nodes in GB"
+  type        = number
+  default     = 1
+}
+
+variable "disk_type" {
+  description = "Disk type for GKE nodes"
+  type        = string
+  default     = "pd-balanced"
+}
+
+variable "preemptible" {
+  description = "Whether to use preemptible VMs for GKE nodes"
+  type        = bool
+  default     = false
+}
+
+variable "create_system_node_pool" {
+  description = "Whether to create a system node pool"
+  type        = bool
+  default     = false
+}
