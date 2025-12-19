b5eaab79e0f573ebbc5f1253d74f662ce8cbd5fe
Laurent Jiang <laurent.jiang@epitech.eu>
2025-12-19 15:17:53 +0100

    feat(network): public and private vpc added, router and nat added

--- terraform/env/prd.tfvars
+++ terraform/env/prd.tfvars
@@ -1,4 +1,17 @@
-project_id  = "epitech-iaas-prod"
+project_id  = "iaasepitech"
 region      = "europe-west9"
 vpc_name = "team5-vpc-prd"
-cidr_block  = "10.0.1.0/24"
+cidr_block  = "10.0.0.0/20"
+pods_range_name = "pods-range"
+services_range_name = "services-range"
+
+cluster_name = "team5-gke-cluster"
+environment = "prd"
+node_count = 2
+min_node_count = 1
+max_node_count = 5
+machine_type = "e2-medium"
+disk_size_gb = 10
+disk_type = "pd-balanced"
+preemptible = false
+create_system_node_pool = false
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,27 +2,31 @@ output "vpc_id" {
   value = google_compute_network.vpc_network.id
 }
 
-output "subnet_id" {
-  value = google_compute_subnetwork.vpc_subnet.id
+output "public_subnet_id" {
+  value = google_compute_subnetwork.public_subnet.id
 }
 
-output "cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "The name of the GKE cluster"
+output "private_subnet_id" {
+  value = google_compute_subnetwork.private_subnet.id
 }
 
-output "cluster_endpoint" {
-  value       = google_container_cluster.primary.endpoint
-  description = "The endpoint of the GKE cluster"
-  sensitive   = true
-}
+# output "cluster_name" {
+#   value       = google_container_cluster.primary.name
+#   description = "The name of the GKE cluster"
+# }
 
-output "cluster_ca_certificate" {
-  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-  description = "The CA certificate of the GKE cluster"
-  sensitive   = true
-}
+# output "cluster_endpoint" {
+#   value       = google_container_cluster.primary.endpoint
+#   description = "The endpoint of the GKE cluster"
+#   sensitive   = true
+# }
 
-output "kubectl_config_command" {
-  value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project_id}"
-}
+# output "cluster_ca_certificate" {
+#   value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+#   description = "The CA certificate of the GKE cluster"
+#   sensitive   = true
+# }
+
+# output "kubectl_config_command" {
+#   value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project_id}"
+# }
