2187082c00713164802729d9d9ba6e981af9a3fc
Tokuuuu <72007811+iTokubz@users.noreply.github.com>
2026-01-15 12:02:51 +0100

    feat/monitoring: add traces, log and alerts, also added an alertmanager ingress to monitor alerts fired, improved graphana dashboards

--- terraform/10-cloudbuild.tf
+++ terraform/10-cloudbuild.tf
@@ -3,17 +3,19 @@ resource "null_resource" "build_and_push_image" {
   # Trigger rebuild when app code changes
   triggers = {
     dockerfile_hash = filesha256("${path.module}/../app/Dockerfile")
+    # Add more app files to watch for changes if needed
+    # go_mod_hash     = filesha256("${path.module}/../app/go.mod")
   }
 
   # Build and push image using Cloud Build
   provisioner "local-exec" {
-    working_dir = "${path.module}/.."
-    command     = <<EOT
-gcloud builds submit \
-  --config=cloudbuild.yaml \
-  --substitutions=_ARTIFACT_REGISTRY_URL=${google_artifact_registry_repository.docker_repo.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id},SHORT_SHA=terraform \
-  --project=${var.project_id} .
-EOT
+    command = <<-EOT
+      gcloud builds submit \
+        --config=${path.module}/../cloudbuild.yaml \
+        --substitutions=_ARTIFACT_REGISTRY_URL="${google_artifact_registry_repository.docker_repo.location}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}",SHORT_SHA="terraform" \
+        --project=${var.project_id} \
+        ${path.module}/../
+    EOT
   }
 
   depends_on = [
